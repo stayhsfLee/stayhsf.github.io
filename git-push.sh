1e8358303092eadfbd840f6dd42b8f5acbf9cc51
tinkericlee <eric.xiangfu.lee@gmail.com>
2021-03-23 10:31:08 +0800

    Use github workflow to deploy pages

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,10 @@
 git config user.email "eric.xiangfu.lee@gmail.com"
 git config user.name "tinkericlee"
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+if [ $BRANCH_NAME != 'source' ]; then
+    echo 'Failed: please change your branch to source branch'
+    exit -1
+fi
 git add *
 if [ -z "$1" ]; then
     git commit -m "[automatic-push] pushed time: $(date)"
@@ -7,4 +12,3 @@ else
     git commit -m "$(echo "[automatic-push] $@")"
 fi
 git push
-
